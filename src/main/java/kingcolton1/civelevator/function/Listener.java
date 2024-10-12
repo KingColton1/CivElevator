ea94447ea08a4985da694f31cb1e2d287d30700e
Colton B <main@coltvel.com>
2024-10-12 14:52:19 -0400

    Added block interaction for clicking

--- src/main/java/kingcolton1/civelevator/function/Listener.java
+++ src/main/java/kingcolton1/civelevator/function/Listener.java
@@ -4,6 +4,7 @@ import com.destroystokyo.paper.event.player.PlayerJumpEvent;
 import kingcolton1.civelevator.config.config;
 import kingcolton1.civelevator.framework.AutoLoad;
 import kingcolton1.civelevator.framework.DataParser;
+import kingcolton1.civelevator.framework.DoubleInteractFixer;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Location;
@@ -14,12 +15,16 @@ import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.event.block.Action;
 
 public class Listener implements org.bukkit.event.Listener {
     @AutoLoad(processor = DataParser.MATERIAL)
     private final Material elevatorBlock = getMaterialName();
     private final Sound elevatorSound = getSoundName();
+
+    private DoubleInteractFixer interactFixer;
 
     private Material getMaterialName() {
         String materialName = config.get().getString("ELEVATOR MATERIAL");
@@ -94,5 +99,46 @@ public class Listener implements org.bukkit.event.Listener {
         String message = String.format("No %s block to teleport you up to. Sneak to teleport down instead.", this.elevatorBlock);
 
         eventJump.getPlayer().sendMessage(Component.text(message).color(NamedTextColor.RED));
+    }
+
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
+	public void elevatorBlockInteract(PlayerInteractEvent eventInteract) {
+        if (eventInteract.getAction() == Action.RIGHT_CLICK_BLOCK) {
+			if (interactFixer.checkInteracted(eventInteract.getPlayer(), eventInteract.getClickedBlock())) {
+				return;
+			}
+		} else if (eventInteract.getAction() != Action.LEFT_CLICK_BLOCK) {
+			return;
+		}
+
+        Block below = eventInteract.getPlayer().getLocation().getBlock().getRelative(BlockFace.DOWN);
+        if (below.getType() != elevatorBlock) {
+			return;
+		}
+		if (eventInteract.getClickedBlock() == null) {
+			return;
+		}
+		if (!eventInteract.getClickedBlock().equals(below)) {
+			return;
+		}
+		if (eventInteract.getAction() == Action.LEFT_CLICK_BLOCK) {
+			for (int y = below.getY() - 1; y > below.getWorld().getMinHeight(); y--) {
+				if (doTeleport(below, eventInteract.getPlayer(), y)) {
+					return;
+				}
+			}
+
+			String message = String.format("No %s block to teleport you down to. Right click to teleport up instead.", this.elevatorBlock);
+			eventInteract.getPlayer().sendMessage(Component.text(message).color(NamedTextColor.RED));
+		} else {
+			for (int y = below.getY() + 1; y <= below.getWorld().getMaxHeight(); y++) {
+				if (doTeleport(below, eventInteract.getPlayer(), y)) {
+					return;
+				}
+			}
+
+			String message = String.format("No %s block to teleport you up to. Left click to teleport down instead.", this.elevatorBlock);
+			eventInteract.getPlayer().sendMessage(Component.text(message).color(NamedTextColor.RED));
+		}
     }
 }
